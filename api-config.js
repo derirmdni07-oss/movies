f8262a132455080920e75e39f94297164d1e10c3
derirmdni07-oss <derirmdni.07@gmail.com>
2026-01-11 13:53:56 +0700

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    API_KEY: '22be462e6d3de1dbab03d1ca50847b5a', // TMDB API
+    CPA_LINK: 'https://playfloowz.com/c?a=275&o=53&sub2=de',       // Link Iklan Anda
+    BASE_URL: 'https://api.themoviedb.org/3',
+    IMG_URL: 'https://image.tmdb.org/t/p/w500',
+    HD_IMG: 'https://image.tmdb.org/t/p/original',
+    SITE_NAME: 'MOVIES & TV SHOWS'
+};
